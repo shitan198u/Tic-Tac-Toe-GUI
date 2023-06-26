8ff9f8276dedd166b5015cc11e9bacc30a234575
Shitanshu Shivam <74671269+shitan198u@users.noreply.github.com>
2023-06-26 19:11:23 +0530

    using pyqt to run the game.py

--- game.py
+++ game.py
@@ -1,83 +1,77 @@
-from tkinter import *
-from tkinter import messagebox
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QGridLayout
+import sys
 
-# Create main window
-window = Tk()
-window.title("Tic Tac Toe")
-window.geometry("600x600")
-window.resizable(True, True)
+class TicTacToe(QMainWindow):
+    def __init__(self):
+        super(TicTacToe, self).__init__()
+        self.setWindowTitle("Tic Tac Toe")
+        self.setGeometry(200, 200, 350, 350)
+        self.button_list = []
+        self.player = "X"
+        self.initUI()
 
-# Create GUI elements
-frame = Frame(window, bg="#2A2E32")
-frame.pack(fill=BOTH, expand=True)
+    def initUI(self):
+        central_widget = QtWidgets.QWidget()
+        layout = QGridLayout()
+        for i in range(3):
+            button_row = []
+            for j in range(3):
+                button = QtWidgets.QPushButton()
+                button.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+                button.clicked.connect(lambda _, button=button: self.click_handler(button))
+                layout.addWidget(button, i, j)
+                layout.setRowStretch(i, 1)
+                layout.setColumnStretch(j, 1)
+                button_row.append(button)
+            self.button_list.append(button_row)
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
 
-button_list = []
-player = "X"
-
-# Define function to check for a win
-def check_win():
-    # Check rows for win
-    for row in button_list:
-        if row[0]["text"] == row[1]["text"] == row[2]["text"] != "":
+    def check_win(self):
+        # Check rows for win
+        for row in self.button_list:
+            if row[0].text() == row[1].text() == row[2].text() != "":
+                return True
+        # Check columns for win
+        for i in range(3):
+            if self.button_list[0][i].text() == self.button_list[1][i].text() == self.button_list[2][i].text() != "":
+                return True
+        # Check diagonals for win
+        if self.button_list[0][0].text() == self.button_list[1][1].text() == self.button_list[2][2].text() != "":
             return True
-    # Check columns for win
-    for i in range(3):
-        if button_list[0][i]["text"] == button_list[1][i]["text"] == button_list[2][i]["text"] != "":
+        if self.button_list[0][2].text() == self.button_list[1][1].text() == self.button_list[2][0].text() != "":
             return True
-    # Check diagonals for win
-    if button_list[0][0]["text"] == button_list[1][1]["text"] == button_list[2][2]["text"] != "":
-        return True
-    if button_list[0][2]["text"] == button_list[1][1]["text"] == button_list[2][0]["text"] != "":
-        return True
-    return False
+        return False
 
-# Define function to handle button clicks
-def click_handler(button):
-    global player
-    if button["text"] == "":
-        button["text"] = player
-        if check_win():
-            messagebox.showinfo("Game Over", "Player {} wins!".format(player))
-            window.update()
-            if messagebox.askyesno("Game Over", "Do you want to play again?"):
-                reset_game()
+    def click_handler(self, button):
+        if button.text() == "":
+            button.setText(self.player)
+            if self.check_win():
+                QMessageBox.information(self, "Game Over", "Player {} wins!".format(self.player))
+                if QMessageBox.question(self, "Game Over", "Do you want to play again?", QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
+                    self.reset_game()
+                else:
+                    sys.exit()
+            elif all(button.text() != "" for row in self.button_list for button in row):
+                QMessageBox.information(self, "Game Over", "It's a tie!")
+                if QMessageBox.question(self, "Game Over", "Do you want to play again?", QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
+                    self.reset_game()
+                else:
+                    sys.exit()
             else:
-                window.destroy()
-        elif all(button["text"] != "" for row in button_list for button in row):
-            messagebox.showinfo("Game Over", "It's a tie!")
-            window.update()
-            if messagebox.askyesno("Game Over", "Do you want to play again?"):
-                reset_game()
-            else:
-                window.destroy()
-        else:
-            player = "O" if player == "X" else "X"
+                self.player = "O" if self.player == "X" else "X"
 
-# Define function to reset the game
-def reset_game():
-    global player
-    player = "X"
-    for row in button_list:
-        for button in row:
-            button["text"] = ""
+    def reset_game(self):
+        self.player = "X"
+        for row in self.button_list:
+            for button in row:
+                button.setText("")
 
-# Create the buttons
-for i in range(3):
-    button_row = []
-    for j in range(3):
-        button = Button(frame, bg="#3E4347", fg="white", width=10, height=5, font=("Arial", 24, "bold"))
-        button.grid(row=i, column=j, padx=10, pady=10, sticky=NSEW)
-        button_row.append(button)
-        
-        # Make buttons resizable
-        frame.grid_rowconfigure(i, weight=1)
-        frame.grid_columnconfigure(j, weight=1)
-        button.grid(sticky=NSEW)
-        
-        # Bind the button to the click handler
-        button.config(command=lambda button=button: click_handler(button))
-        
-    button_list.append(button_row)
+def window():
+    app = QApplication(sys.argv)
+    win = TicTacToe()
+    win.show()
+    sys.exit(app.exec_())
 
-# Run the main event loop
-window.mainloop()
+window()
